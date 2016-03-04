56540f12a44acb74a07dcd7fa3325b50e61e2d7f
hillakatki <hillakatki@gmail.com>
2016-03-04 17:24:10 -0500

    Sources are displaying from sources file
    - Still no JSON (need to figure out how to hook that up)
    - Sources are repeating; will tackle that next

--- js/index.js
+++ js/index.js
@@ -2,41 +2,108 @@
 
 
 
-
-// var $container = $('#container');
-
-// // initialize Masonry after all images have loaded  
-// $container.imagesLoaded( function() {
-//      $container.masonry();
-// });
-
-$(window).load(function(){
-	$('#grid').masonry({
-	  columnWidth: 300,
-	  gutter: 40,
-	  itemSelector: ".grid-item"
-	});
-});
-
 var letters = {
-	"a" : ["a_01.jpg","a_02.jpg","a_03.jpg"],
-	"b" : ["b_01.jpg","b_02.jpg","b_03.jpg"],
-	"c" : ["c_01.jpg","c_02.jpg","c_03.jpg"],
-	"d" : ["d_01.jpg","d_02.jpg","d_03.jpg"],
+	"a" : ["a_01","a_02","a_03"],
+	"b" : ["b_01","b_02","b_03"],
+	"c" : ["c_01","c_02","c_03"],
+	"d" : ["d_01","d_02","d_03"],
 };
 
-var text = "aaa bbbb cc ddddd a bb cccccc ddd";
+
+
+
+
+
+
+var sources = {
+  "a_01": {
+    "title": "DAILY BREAKFAST [held by] MANSION [at] [BUFFALO, NY] ([COFFEE SHOP])",
+    "date": "1851-1860",
+    "collection": "Buttolph Collection of Menus",
+    "document_link": "http://digitalcollections.nypl.org/items/510d47db-1a07-a3d9-e040-e00a18064a99",
+    "document_name": "nypl.digitalcollections.510d47db-1a07-a3d9-e040-e00a18064a99.001.g.jpg"
+  },
+  "a_02": {
+    "title": "Kampf im zwielicht.",
+    "date": "1926-1947",
+    "collection": "Collection of Book Jackets",
+    "document_link": "http://digitalcollections.nypl.org/items/510d47db-d1c7-a3d9-e040-e00a18064a99",
+    "document_name": "nypl.digitalcollections.510d47db-d1c7-a3d9-e040-e00a18064a99.001.w.jpg"
+  },
+  "a_03": {
+    "title": "Poster for the Revue Rodgers & Hart",
+    "date": 1975,
+    "collection": "Billy Rose Theater Division Scrapbooks",
+    "document_link": "http://digitalcollections.nypl.org/items/7f33d937-18b8-da4d-e040-e00a180604aa",
+    "document_name": "nypl.digitalcollections.7f33d937-18b8-da4d-e040-e00a180604aa.001.w.jpg"
+  },
+  "b_01": {
+    "title": "DAILY BREAKFAST [held by] MANSION [at] [BUFFALO, NY] ([COFFEE SHOP])",
+    "date": "1851-1865",
+    "collection": "Buttolph Collection of Menus",
+    "document_link": "http://digitalcollections.nypl.org/items/510d47db-1a07-a3d9-e040-e00a18064a99",
+    "document_name": "nypl.digitalcollections.510d47db-1a07-a3d9-e040-e00a18064a99.001.g.jpg"
+  },
+  "b_02": {
+    "title": "Harper's September",
+    "date": "189-?-1918",
+    "collection": "Posters. New York, 1890s-1907",
+    "document_link": "http://digitalcollections.nypl.org/items/510d47dc-4868-a3d9-e040-e00a18064a99",
+    "document_name": "nypl.digitalcollections.510d47dc-4868-a3d9-e040-e00a18064a99.001.w.jpg"
+  },
+  "b_03": {
+    "title": "Opening of main text. Rubrics.",
+    "date": 1563,
+    "collection": "Funff Osterpredigten aus dem Gesichte Ezechielis am 37 Capittel gethan.",
+    "document_link": "http://digitalcollections.nypl.org/items/510d47da-e998-a3d9-e040-e00a18064a99",
+    "document_name": "nypl.digitalcollections.510d47da-e998-a3d9-e040-e00a18064a99.001.w.jpg"
+  },
+  "c_01": {
+    "title": "Kampf im zwielicht.",
+    "date": "1926-1948",
+    "collection": "Collection of Book Jackets",
+    "document_link": "http://digitalcollections.nypl.org/items/510d47db-d1c7-a3d9-e040-e00a18064a99",
+    "document_name": "nypl.digitalcollections.510d47db-d1c7-a3d9-e040-e00a18064a99.001.w.jpg"
+  },
+  "c_02": {
+    "title": "Stockholm Restaurant",
+    "date": 1963,
+    "collection": "Buttolph Collection of Menus",
+    "document_link": "http://digitalcollections.nypl.org/items/5e66b3e8-f9ac-d471-e040-e00a180654d7",
+    "document_name": "stockholm_restaurant.png"
+  },
+  "c_03": {
+    "title": "Alphabet du temps de l'Abbe Suger, tire du cabinet de M. Debret",
+    "date": 1874,
+    "collection": "Recueil de cent planches de décoration : extraites du Journal-manuel de peintures",
+    "document_link": "http://digitalcollections.nypl.org/items/510d47da-3b26-a3d9-e040-e00a18064a99",
+    "document_name": "nypl.digitalcollections.510d47da-3b26-a3d9-e040-e00a18064a99.001.w.jpg"
+  }
+};
+
+
+
+
+
+var text = "aaa b cccc aa bb cccccc";
 var letterArray;
 var imageName;
 var wordCount = 0;
 
+// get json
+// var sources;
+// $.getJSON('json/sources.json', function(json){
+// 	console.log(json.imageName);
+// });
+
 
 for (var i = 0; i < text.length; i++) {
 	console.log("i: "+i);
+	//if the letter is a space, start a new word by appending a word div
 	if (text[i] === " ") {
 		$(".quote-box").append('<div class="word"></div>');
 		wordCount++;
-		console.log("space!");
+		// console.log("space!");
 	}else {
 		letterArray = letters[text[i]];
 		// console.log(letterArray);
@@ -45,11 +112,24 @@ for (var i = 0; i < text.length; i++) {
 		// console.log(randomNum);
 		imageName = letterArray[randomNum];
 		console.log(imageName);
-		$(".word").eq(wordCount).append('<div class="letter"><img src="images/letters/'+imageName+'"/></div>');
-		// .animate({
-		// 	'opacity':1
-		// }, 3000);
+		$(".word").eq(wordCount).append('<div class="letter"><img src="images/letters/'+imageName+'.jpg"/></div>');
+
+		//getting the array of metadata associated with the letter:
+		var letterSource = sources[imageName];
+		$("#grid").append('<div class="grid-item"><img src="images/originals/'+letterSource.document_name+'" width="300"/><div class="metadata"><span class="source-title">Title: </span>'+letterSource.title+'</div><div class="metadata"><span class="source-date">Date: </span>'+letterSource.date+'</div><div class="metadata"><span class="source-collection">Collection: </span>'+letterSource.collection+'</div></div>');
+
+		console.log(letterSource.title);
+
+
+			// get source : data[ letterArray[randomNum] ]
 	};
 
 };
 
+$(window).load(function(){
+	$('#grid').masonry({
+	  columnWidth: 300,
+	  gutter: 40,
+	  itemSelector: ".grid-item"
+	});
+});
