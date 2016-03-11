a2957a2700ca778df919f9610ba173900e121389
hillakatki <hillakatki@gmail.com>
2016-03-11 01:37:36 -0500

    hooked up quote button
    only works on first click. does not work after that.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,18 @@
 
-
+var newQuote;
+var buttonClicks = 0;
 //on click of the quote button:
 $('.quote-button').on('click', function(){
+	//clear out previous quote
+	buttonClicks++;
+	console.log('button clicked:'+buttonClicks);
+
+	$('.quote-box').html('<div class="word"></div>');
+	console.log('just emptied quote box');
+
+	$('.author').empty();
+	$('.title').empty();
+
 	//random quote generator; need to use Object.keys to get the length of the object
 	var randomNum2 = Math.floor(Math.random() * Object.keys(quotes).length);
 	//choose a quote, put its metadata in an array
@@ -11,17 +22,31 @@ $('.quote-button').on('click', function(){
 	console.log(quoteArray.quote);
 	console.log(quoteArray.author);
 	console.log(quoteArray.title);
+	
+	newQuote = quoteArray.quote;
+	quoteDisplay(newQuote);
+
+	$('.author').append(quoteArray.author);
+	$('.title').append(quoteArray.title);
+
+	//Will call the sourceDisplay function only when the images are loaded
+	$('#grid').imagesLoaded(function(){
+		sourceDisplay();
+	});
+
+
 });
 
 
 
 //declaring variables
-var text = "one must be careful of books and what is inside them for words have the power to change us";
+// var text = "one must be careful of books and what is inside them for words have the power to change us";
 var letterArray;
 var imageName;
 var wordCount = 0;
 var currentSourceNames = [];
 var sourceCount = 0;
+
 
 // get json
 // var sources;
@@ -32,54 +57,68 @@ var sourceCount = 0;
 
 
 //running a for loop for the length of the text
-for (var i = 0; i < text.length; i++) {
-	console.log("main loop, i: "+i);
-	//if the character is a space, start a new word by appending a div called word withing the div called quote-box
-	if (text[i] === " ") {
-		$(".quote-box").append('<div class="word"></div>');
-		wordCount++;
-	}else {
-		//if the character is a letter, find the array for that letter
-		letterArray = letters[text[i]];
-		//generate a random number
-		var randomNum = Math.floor(Math.random() * letterArray.length);
-		//use that random number to generate a specific letter name
-		imageName = letterArray[randomNum];
-		//append a div called letter with the image name concatenated in it
-		$(".word").eq(wordCount).append('<div class="letter"><img src="images/letters/'+imageName+'.jpg"/></div>');
-		//get the array of metadata associated with the letter:
-		var letterSource = sources[imageName];
+function quoteDisplay (text) {
+	
+	for (var i = 0; i < text.length; i++) {
+		//if the character is a space, start a new word by appending a div called word withing the div called quote-box
+		if (text[i] === " ") {
+			$(".quote-box").append('<div class="word"></div>');
+			console.log("word div appended");
 
-		//check if doument sources are the same
-		var sameSource = false;
-		//running a for loop for the length of the array containing the sources currently being used
-		for (var n = 0; n < currentSourceNames.length; n++) {
+			wordCount++;
+		}else {
+			//if the character is a letter, find the array for that letter
+			letterArray = letters[text[i]];
+			//generate a random number
+			var randomNum = Math.floor(Math.random() * letterArray.length);
+			//use that random number to generate a specific letter name
+			imageName = letterArray[randomNum];
+			//append a div called letter with the image name concatenated in it
+			$(".word").eq(wordCount).append('<div class="letter"><img src="images/letters/'+imageName+'.jpg"/></div>');
+			//get the array of metadata associated with the letter:
+			console.log("letter div appended");
 
-			//if the source in the current array is the same the new source name, then it's the same source
-			if (currentSourceNames[n] === letterSource.document_name) {
-				sameSource = true;
+			var letterSource = sources[imageName];
+
+			//check if doument sources are the same
+			var sameSource = false;
+			//running a for loop for the length of the array containing the sources currently being used
+			for (var n = 0; n < currentSourceNames.length; n++) {
+
+				//if the source in the current array is the same the new source name, then it's the same source
+				if (currentSourceNames[n] === letterSource.document_name) {
+					sameSource = true;
+				};
 			};
-		};
-		//if the new source is not the same as any of the sources in the current array
-		if (sameSource === false) {
-			//add this source name to the current source name array
-			currentSourceNames[sourceCount] = letterSource.document_name;
-			//increase the count of sources
-			sourceCount++;
-			//append the source image, title, date, and collection name within a div called grid-item, to a div called grid
-			$("#grid").append('<div class="grid-item"><img src="images/originals/'+letterSource.document_name+'" width="300"/><div class="metadata"><span class="source-title">Title: </span>'+letterSource.title+'</div><div class="metadata"><span class="source-date">Date: </span>'+letterSource.date+'</div><div class="metadata"><span class="source-collection">Collection: </span>'+letterSource.collection+'</div></div>');
+			//if the new source is not the same as any of the sources in the current array
+			if (sameSource === false) {
+				//add this source name to the current source name array
+				currentSourceNames[sourceCount] = letterSource.document_name;
+				//increase the count of sources
+				sourceCount++;
+				//append the source image, title, date, and collection name within a div called grid-item, to a div called grid
+				$("#grid").append('<div class="grid-item"><img src="images/originals/'+letterSource.document_name+'" width="300"/><div class="metadata"><span class="source-title">Title: </span>'+letterSource.title+'</div><div class="metadata"><span class="source-date">Date: </span>'+letterSource.date+'</div><div class="metadata"><span class="source-collection">Collection: </span>'+letterSource.collection+'</div></div>');
+
+			};
 
 		};
 
 	};
+	console.log('end of quoteDisplay');
 
 };
 
 //use the masonry plugin to generate the grid of sources
-$(window).load(function(){
-	$('#grid').masonry({
-	  columnWidth: 300,
-	  gutter: 40,
-	  itemSelector: ".grid-item"
-	});
-});
+function sourceDisplay () {
+	
+	// $(window).load(function(){
+		console.log('in the sourceDisplay function now')
+		$('#grid').masonry({
+		  columnWidth: 300,
+		  gutter: 40,
+		  itemSelector: ".grid-item"
+		});
+	// });
+};
+
+
--- js/quotes.js
+++ js/quotes.js
@@ -1,197 +1,197 @@
 var quotes = {
   "0": {
-    "quote": "In our village, folks say God crumbles up the old moon into stars.",
+    "quote": "in our village folks say god crumbles up the old moon into stars",
     "author": "Alexander Solzhenitsyn",
     "title": "One Day in the Life of Ivan Denisovich"
   },
   "1": {
-    "quote": "America, I've given you all and now I'm nothing.",
+    "quote": "america ive given you all and now im nothing",
     "author": "Allen Ginsburg",
     "title": "America"
   },
   "2": {
-    "quote": "In spite of everything, I still believe people are really good at heart.",
+    "quote": "in spite of everything i still believe people are really good at heart",
     "author": "Anne Frank",
     "title": "The Diary of Anne Frank"
   },
-  "3": {
-    "quote": "As Estha stirred the thick jam he thought Two Thoughts and the Two Thoughts he thought were these: a) Anything can happen to anyone. and b) It is best to be prepared.",
-    "author": "Arundhati Roy",
-    "title": "The God of Small Things"
-  },
-  "4": {
-    "quote": "‘Dear God,' she prayed, ‘let me be something every minute of every hour of my life.'",
-    "author": "Betty Smith",
-    "title": "A Tree Grows in Brooklyn"
-  },
-  "5": {
-    "quote": "There are darknesses in life and there are lights, and you are one of the lights, the light of all lights.",
-    "author": "Bram Stroker",
-    "title": "Dracula"
-  },
-  "6": {
-    "quote": "One must be careful of books, and what is inside them, for words have the power to change us.",
-    "author": "Cassandra Clare",
-    "title": "The Infernal Devices"
-  },
-  "7": {
-    "quote": "A dream, all a dream, that ends in nothing, and leaves the sleeper where he lay down, but I wish you to know that you inspired it.",
-    "author": "Charles Dickens",
-    "title": "A Tale of Two Cities"
-  },
-  "8": {
-    "quote": "I would always rather be happy than dignified.",
-    "author": "Charlotte Brontë",
-    "title": "Jane Eyre"
-  },
-  "9": {
-    "quote": "How wild it was, to let it be.",
-    "author": "Cheryl Strayed",
-    "title": "Wild"
-  },
-  "10": {
-    "quote": "It frightened him to think what must have gone to the making of her eyes.",
-    "author": "Edith Wharton",
-    "title": "The Age of Innocence"
-  },
-  "11": {
-    "quote": "She was lost in her longing to understand.",
-    "author": "Gabriel Garcia Marquez",
-    "title": "Love in the Time of Cholera"
-  },
-  "12": {
-    "quote": "She wasn't doing a thing that I could see, except standing there leaning on the balcony railing, holding the universe together.",
-    "author": "J. D. Salinger",
-    "title": "A Girl I Knew"
-  },
-  "13": {
-    "quote": "It does not do well to dwell on dreams and forget to live, remember that.",
-    "author": "J.K. Rowling",
-    "title": "Harry Potter and the Sorcerer's Stone"
-  },
-  "14": {
-    "quote": "What are men to rocks and mountains?",
-    "author": "Jane Austen",
-    "title": "Pride and Prejudice"
-  },
-  "15": {
-    "quote": "And now that you don't have to be perfect, you can be good.",
-    "author": "John Steinbeck",
-    "title": "East of Eden"
-  },
-  "16": {
-    "quote": "Sometimes I can feel my bones straining under the weight of all the lives I'm not living.",
-    "author": "Jonathan Safran Foer",
-    "title": "Extremely Loud and Incredibly Close"
-  },
-  "17": {
-    "quote": "The half life of love is forever.",
-    "author": "Junot Diaz",
-    "title": "This Is How You Lose Her"
-  },
-  "18": {
-    "quote": "She was becoming herself and daily casting aside that fictitious self which we assume like a garment with which to appear before the world.",
-    "author": "Kate Chopin",
-    "title": "The Awakening"
-  },
-  "19": {
-    "quote": "Beauty is an enormous, unmerited gift given randomly, stupidly.",
-    "author": "Khaled Hosseini",
-    "title": "And the Mountains Echoed"
-  },
-  "20": {
-    "quote": "I wondered if that was how forgiveness budded; not with the fanfare of epiphany, but with pain gathering its things, packing up, and slipping away unannounced in the middle of the night.",
-    "author": "Khaled Hosseini",
-    "title": "The Kite Runner"
-  },
-  "21": {
-    "quote": "Everything was beautiful and nothing hurt.",
-    "author": "Kurt Vonnegut",
-    "title": "Slaughterhouse-Five"
-  },
-  "22": {
-    "quote": "Tomorrow is always fresh, with no mistakes in it yet.",
-    "author": "L. M. Montgomery",
-    "title": "Anne of Green Gables"
-  },
-  "23": {
-    "quote": "For poems are like rainbows; they escape you quickly.",
-    "author": "Langston Hughes",
-    "title": "The Big Sea"
-  },
-  "24": {
-    "quote": "Once upon a time there was a boy who loved a girl, and her laughter was a question he wanted to spend his whole life answering.",
-    "author": "Nicole Krauss",
-    "title": "The History of Love"
-  },
-  "25": {
-    "quote": "The curves of your lips rewrite history.",
-    "author": "Oscar Wilde",
-    "title": "The Picture of Dorian Gray"
-  },
-  "26": {
-    "quote": "I could hear the human noise we sat there making, not one of us moving, not even when the room went dark.",
-    "author": "Raymond Carver",
-    "title": "What We Talk About When We Talk About Love"
-  },
-  "27": {
-    "quote": "I took a deep breath and listened to the old brag of my heart; I am, I am, I am.",
-    "author": "Sylvia Plath",
-    "title": "The Bell Jar"
-  },
-  "28": {
-    "quote": "At the still point, there the dance is.",
-    "author": "T. S. Eliot",
-    "title": "Four Quartets"
-  },
-  "29": {
-    "quote": "Do I dare / Disturb the universe?",
-    "author": "T. S. Eliot",
-    "title": "The Love Song of J. Alfred Prufrock"
-  },
-  "30": {
-    "quote": "So we beat on, boats against the current, borne back ceaselessly into the past.",
-    "author": "The Great Gatsby",
-    "title": "F. Scott Fitzgerald"
-  },
-  "31": {
-    "quote": "We cross our bridges as we come to them and burn them behind us, with nothing to show for our progress except a memory of the smell of smoke, and the presumption that once our eyes watered.",
-    "author": "Tom Stoppard",
-    "title": "Rosencratz and Guildenstern Are Dead"
-  },
-  "32": {
-    "quote": "The pieces I am, she gather them and gave them back to me in all the right order.",
-    "author": "Toni Morrison",
-    "title": "Beloved"
-  },
-  "33": {
-    "quote": "I have spread my dreams under your feet; / Tread softly because you tread on my dreams",
-    "author": "W. B. Yeats",
-    "title": "Aedh Wishes for the Cloths of Heaven"
-  },
-  "34": {
-    "quote": "If equal affection cannot be, let the more loving one be me.",
-    "author": "W. H. Auden",
-    "title": "The More Loving One"
-  },
-  "35": {
-    "quote": "It might be that to surrender to happiness was to accept defeat, but it was a defeat better than many victories.",
-    "author": "W. Somerset Maugham",
-    "title": "Of Human Bondage"
-  },
-  "36": {
-    "quote": "I celebrate myself, and sing myself.",
-    "author": "Walt Whitman",
-    "title": "Leaves of Grass"
-  },
-  "37": {
-    "quote": "There are more things in Heaven and Earth, Horatio, than are dreamt of in your philosophy.",
-    "author": "William Shakespeare",
-    "title": "Hamlet"
-  },
-  "38": {
-    "quote": "Journeys end in lovers meeting.",
-    "author": "William Shakespeare",
-    "title": "Twelfth Night"
-  }
+  // "3": {
+  //   "quote": "As Estha stirred the thick jam he thought Two Thoughts and the Two Thoughts he thought were these: a) Anything can happen to anyone. and b) It is best to be prepared.",
+  //   "author": "Arundhati Roy",
+  //   "title": "The God of Small Things"
+  // },
+  // "4": {
+  //   "quote": "‘Dear God,' she prayed, ‘let me be something every minute of every hour of my life.'",
+  //   "author": "Betty Smith",
+  //   "title": "A Tree Grows in Brooklyn"
+  // },
+  // "5": {
+  //   "quote": "There are darknesses in life and there are lights, and you are one of the lights, the light of all lights.",
+  //   "author": "Bram Stroker",
+  //   "title": "Dracula"
+  // },
+  // "6": {
+  //   "quote": "One must be careful of books, and what is inside them, for words have the power to change us.",
+  //   "author": "Cassandra Clare",
+  //   "title": "The Infernal Devices"
+  // },
+  // "7": {
+  //   "quote": "A dream, all a dream, that ends in nothing, and leaves the sleeper where he lay down, but I wish you to know that you inspired it.",
+  //   "author": "Charles Dickens",
+  //   "title": "A Tale of Two Cities"
+  // },
+  // "8": {
+  //   "quote": "I would always rather be happy than dignified.",
+  //   "author": "Charlotte Brontë",
+  //   "title": "Jane Eyre"
+  // },
+  // "9": {
+  //   "quote": "How wild it was, to let it be.",
+  //   "author": "Cheryl Strayed",
+  //   "title": "Wild"
+  // },
+  // "10": {
+  //   "quote": "It frightened him to think what must have gone to the making of her eyes.",
+  //   "author": "Edith Wharton",
+  //   "title": "The Age of Innocence"
+  // },
+  // "11": {
+  //   "quote": "She was lost in her longing to understand.",
+  //   "author": "Gabriel Garcia Marquez",
+  //   "title": "Love in the Time of Cholera"
+  // },
+  // "12": {
+  //   "quote": "She wasn't doing a thing that I could see, except standing there leaning on the balcony railing, holding the universe together.",
+  //   "author": "J. D. Salinger",
+  //   "title": "A Girl I Knew"
+  // },
+  // "13": {
+  //   "quote": "It does not do well to dwell on dreams and forget to live, remember that.",
+  //   "author": "J.K. Rowling",
+  //   "title": "Harry Potter and the Sorcerer's Stone"
+  // },
+  // "14": {
+  //   "quote": "What are men to rocks and mountains?",
+  //   "author": "Jane Austen",
+  //   "title": "Pride and Prejudice"
+  // },
+  // "15": {
+  //   "quote": "And now that you don't have to be perfect, you can be good.",
+  //   "author": "John Steinbeck",
+  //   "title": "East of Eden"
+  // },
+  // "16": {
+  //   "quote": "Sometimes I can feel my bones straining under the weight of all the lives I'm not living.",
+  //   "author": "Jonathan Safran Foer",
+  //   "title": "Extremely Loud and Incredibly Close"
+  // },
+  // "17": {
+  //   "quote": "The half life of love is forever.",
+  //   "author": "Junot Diaz",
+  //   "title": "This Is How You Lose Her"
+  // },
+  // "18": {
+  //   "quote": "She was becoming herself and daily casting aside that fictitious self which we assume like a garment with which to appear before the world.",
+  //   "author": "Kate Chopin",
+  //   "title": "The Awakening"
+  // },
+  // "19": {
+  //   "quote": "Beauty is an enormous, unmerited gift given randomly, stupidly.",
+  //   "author": "Khaled Hosseini",
+  //   "title": "And the Mountains Echoed"
+  // },
+  // "20": {
+  //   "quote": "I wondered if that was how forgiveness budded; not with the fanfare of epiphany, but with pain gathering its things, packing up, and slipping away unannounced in the middle of the night.",
+  //   "author": "Khaled Hosseini",
+  //   "title": "The Kite Runner"
+  // },
+  // "21": {
+  //   "quote": "Everything was beautiful and nothing hurt.",
+  //   "author": "Kurt Vonnegut",
+  //   "title": "Slaughterhouse-Five"
+  // },
+  // "22": {
+  //   "quote": "Tomorrow is always fresh, with no mistakes in it yet.",
+  //   "author": "L. M. Montgomery",
+  //   "title": "Anne of Green Gables"
+  // },
+  // "23": {
+  //   "quote": "For poems are like rainbows; they escape you quickly.",
+  //   "author": "Langston Hughes",
+  //   "title": "The Big Sea"
+  // },
+  // "24": {
+  //   "quote": "Once upon a time there was a boy who loved a girl, and her laughter was a question he wanted to spend his whole life answering.",
+  //   "author": "Nicole Krauss",
+  //   "title": "The History of Love"
+  // },
+  // "25": {
+  //   "quote": "The curves of your lips rewrite history.",
+  //   "author": "Oscar Wilde",
+  //   "title": "The Picture of Dorian Gray"
+  // },
+  // "26": {
+  //   "quote": "I could hear the human noise we sat there making, not one of us moving, not even when the room went dark.",
+  //   "author": "Raymond Carver",
+  //   "title": "What We Talk About When We Talk About Love"
+  // },
+  // "27": {
+  //   "quote": "I took a deep breath and listened to the old brag of my heart; I am, I am, I am.",
+  //   "author": "Sylvia Plath",
+  //   "title": "The Bell Jar"
+  // },
+  // "28": {
+  //   "quote": "At the still point, there the dance is.",
+  //   "author": "T. S. Eliot",
+  //   "title": "Four Quartets"
+  // },
+  // "29": {
+  //   "quote": "Do I dare / Disturb the universe?",
+  //   "author": "T. S. Eliot",
+  //   "title": "The Love Song of J. Alfred Prufrock"
+  // },
+  // "30": {
+  //   "quote": "So we beat on, boats against the current, borne back ceaselessly into the past.",
+  //   "author": "The Great Gatsby",
+  //   "title": "F. Scott Fitzgerald"
+  // },
+  // "31": {
+  //   "quote": "We cross our bridges as we come to them and burn them behind us, with nothing to show for our progress except a memory of the smell of smoke, and the presumption that once our eyes watered.",
+  //   "author": "Tom Stoppard",
+  //   "title": "Rosencratz and Guildenstern Are Dead"
+  // },
+  // "32": {
+  //   "quote": "The pieces I am, she gather them and gave them back to me in all the right order.",
+  //   "author": "Toni Morrison",
+  //   "title": "Beloved"
+  // },
+  // "33": {
+  //   "quote": "I have spread my dreams under your feet; / Tread softly because you tread on my dreams",
+  //   "author": "W. B. Yeats",
+  //   "title": "Aedh Wishes for the Cloths of Heaven"
+  // },
+  // "34": {
+  //   "quote": "If equal affection cannot be, let the more loving one be me.",
+  //   "author": "W. H. Auden",
+  //   "title": "The More Loving One"
+  // },
+  // "35": {
+  //   "quote": "It might be that to surrender to happiness was to accept defeat, but it was a defeat better than many victories.",
+  //   "author": "W. Somerset Maugham",
+  //   "title": "Of Human Bondage"
+  // },
+  // "36": {
+  //   "quote": "I celebrate myself, and sing myself.",
+  //   "author": "Walt Whitman",
+  //   "title": "Leaves of Grass"
+  // },
+  // "37": {
+  //   "quote": "There are more things in Heaven and Earth, Horatio, than are dreamt of in your philosophy.",
+  //   "author": "William Shakespeare",
+  //   "title": "Hamlet"
+  // },
+  // "38": {
+  //   "quote": "Journeys end in lovers meeting.",
+  //   "author": "William Shakespeare",
+  //   "title": "Twelfth Night"
+  // }
 };
